fdf1751d811044fb8125231598c285f9c33446b5
ALI\Asus <alisoleimani199@gmail.com>
2021-02-28 13:44:14 +0330

    Complate rulse section

---
+++ js/events.js
@@ -0,0 +1,8 @@
+function rulse()  {
+    let rulseBtn = document.getElementById("rulse-btn");
+    let rulseContant = document.getElementById("rulse");
+
+    rulseBtn.addEventListener("click", () => rulseContant.style.display = "block");
+}
+
+export default rulse;
---
+++ js/script.js
@@ -0,0 +1,1 @@
+import rulse from "./events.js";
